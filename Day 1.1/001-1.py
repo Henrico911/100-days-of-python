3869f4d1ebe5385e202d7e8628a51caeabcc6fda
Henrico <karamagihenry911@gmail.com>
2024-05-03 11:48:19 +0300

    added folders with files

---
+++ Day 1.1/001-1.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 20 17:40:15 2024
+
+@author: skurge
+"""
+
+print("print('what to print')")
---
+++ Day 1.1/001-2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 20 17:45:06 2024
+
+@author: skurge
+"""
+
+#string concatenation
+print("hello" + " Skurge")
+print("hello" + " " + "Skurge")
+
+
